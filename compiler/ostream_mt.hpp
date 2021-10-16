5e38639b812d9611243a15de415ce66acc93d47d
Canberk Sönmez <canberk.sonmez.409@gmail.com>
2021-10-16 21:47:11 +0200

    Thread-safe wrappers for ostreams.

---
+++ compiler/ostream_mt.hpp
@@ -0,0 +1,43 @@
+/**
+ * @file ostream_mt.hpp
+ * @brief Provides wrappers to make ostream's thread-safe.
+ * @author Canberk Sonmez <canberk.sonmez@epfl.ch>
+ * 
+ */
+
+#ifndef OSTREAM_MT
+#define OSTREAM_MT
+
+#include <ios>
+#include <sstream>
+#include <mutex>
+
+struct ostream_mt {
+    ostream_mt(std::ostream &os): os_{os} {
+    }
+
+    auto operator()() {
+        return wrapper{*this};
+    }
+
+private:
+    struct wrapper: std::ostringstream {
+        wrapper(ostream_mt &osmt): osmt_{osmt} {
+        }
+
+        ~wrapper() {
+            std::unique_lock<std::mutex> lock{osmt_.mtx_};
+            osmt_.os_ << this->str();
+            osmt_.os_.flush();
+        }
+    private:
+        ostream_mt &osmt_;
+    };
+
+    std::ostream &os_;
+    std::mutex mtx_;
+};
+
+extern ostream_mt cout_mt;
+
+#endif // OSTREAM_MT
